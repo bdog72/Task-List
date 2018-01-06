24b006e0b86bc8579706013f8572feb36a674197
Brian Bycynski <high5bri@hotmail.com>
2018-01-06 06:18:26 -0500

    final commit

--- app.js
+++ app.js
@@ -1,153 +1,74 @@
-// Define UI Variables
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none'
 
-const form = document.querySelector('#task-form')
-const taskList = document.querySelector('.collection')
-const clearBtn = document.querySelector('.clear-tasks')
-const filter = document.querySelector('#filter')
-const taskInput = document.querySelector('#task')
+    // Show loader
+    document.getElementById('loading').style.display = 'block'
 
-// Load all event listeners
-
-loadEventListeners()
-
-function loadEventListeners() {
-    // DOM Load event
-    document.addEventListener('DOMContentLoaded', getTasks)
-    // Add task event
-    form.addEventListener('submit', addTask)
-    // Remove task event
-    taskList.addEventListener('click', removeTask)
-    // Clear task event
-    clearBtn.addEventListener('click', clearTasks)
-    // Filter tasks event
-    filter.addEventListener('keyup', filterTasks)
-}
-
-// Get Tasks from LS
-function getTasks() {
-    let tasks
-    if (localStorage.getItem('tasks') === null) {
-        tasks = []
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'))
-    }
-    tasks.forEach(function(task) {
-        // Create li element
-        const li = document.createElement('li')
-        // Add class
-        li.className = 'collection-item'
-        // Create text node and append to li
-        li.appendChild(document.createTextNode(task))
-        // Create new link element
-        const link = document.createElement('a')
-        // Add class
-        link.className = 'delete-item secondary-content'
-        // Add icon html
-        link.innerHTML = '<i class="fa fa-remove"></i>'
-        // Append the link to li
-        li.appendChild(link)
-        // Append li to ul
-        taskList.appendChild(li)
-    })
-}
-
-// Add Task
-function addTask(e) {
-    if (taskInput.value === '') {
-        alert('Add A Task')
-    }
-
-    // Create li element
-    const li = document.createElement('li')
-    // Add class
-    li.className = 'collection-item'
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value))
-    // Create new link element
-    const link = document.createElement('a')
-    // Add class
-    link.className = 'delete-item secondary-content'
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>'
-    // Append the link to li
-    li.appendChild(link)
-    // Append li to ul
-    taskList.appendChild(li)
-    // Store in LS
-    storeTaskInLocalStorage(taskInput.value)
-    // Clear input
-    taskInput.value = ''
+    setTimeout(calculateResults, 2000)
 
     e.preventDefault()
-}
+})
 
-// Store task
+// Caluclate Results
+function calculateResults(e) {
+    console.log('Calculating..')
+    // UI variables
 
-function storeTaskInLocalStorage(task) {
-    let tasks
-    if (localStorage.getItem('tasks') === null) {
-        tasks = []
+    const amount = document.getElementById('amount')
+    const interest = document.getElementById('interest')
+    const years = document.getElementById('years')
+    const monthlyPayment = document.getElementById('monthly-payment')
+    const totalPayment = document.getElementById('total-payment')
+    const totalInterest = document.getElementById('total-interest')
+
+    const principal = parseFloat(amount.value)
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12
+    const calculatedPayments = parseFloat(years.value) * 12
+
+    // Compute Monthly Payments
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+    const monthly = principal * x * calculatedInterest / (x - 1)
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = monthly * calculatedPayments.toFixed(2)
+        totalInterest.value = (
+            monthly * calculatedPayments -
+            principal
+        ).toFixed(2)
+        // Show results
+        document.getElementById('results').style.display = 'block'
+        // Hide Loader
+        document.getElementById('loading').style.display = 'none'
     } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'))
-    }
-    tasks.push(task)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
-
-//Remove task
-
-function removeTask(e) {
-    if (e.target.parentElement.classList.contains('delete-item')) {
-        if (confirm('Are You Sure')) {
-            e.target.parentElement.parentElement.remove()
-
-            // Remove from LS
-            removeTaskFromLocalStorage(e.target.parentElement.parentElement)
-        }
+        showError('Please Check Your Numbers')
     }
 }
 
-// Remove from LS
-function removeTaskFromLocalStorage(taskItem) {
-    let tasks
-    if (localStorage.getItem('tasks') === null) {
-        tasks = []
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'))
-    }
-    tasks.forEach(function(task, index) {
-        if (taskItem.textContent === task) {
-            tasks.splice(index, 1)
-        }
-    })
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+// Show Error
+
+function showError(error) {
+    // Show results
+    document.getElementById('results').style.display = 'none'
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none'
+    const errorDiv = document.createElement('div')
+
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('heading')
+
+    errorDiv.className = 'alert alert-danger'
+
+    errorDiv.appendChild(document.createTextNode(error))
+
+    card.insertBefore(errorDiv, heading)
+
+    setTimeout(clearError, 3000)
 }
 
-// Clear tasks
-
-function clearTasks() {
-    // taskList.innerHTML = ''
-
-    // Faster
-
-    while (taskList.firstChild) {
-        taskList.removeChild(taskList.firstChild)
-    }
-    clearTasksFromLocalStorage()
-}
-
-function clearTasksFromLocalStorage() {
-    localStorage.clear()
-}
-
-function filterTasks(e) {
-    const text = e.target.value.toLowerCase()
-    document.querySelectorAll('.collection-item').forEach(function(task) {
-        const item = task.firstChild.textContent
-        if (item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = 'block'
-        } else {
-            task.style.display = 'none'
-        }
-    })
+function clearError() {
+    document.querySelector('.alert').remove()
 }
